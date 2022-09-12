871f71d5ae02f642554aaa96ef0c78acd30403c6
Artem Poliakov <artem4250@gmail.com>
2022-09-12 21:06:49 +0300

    task1.rb

---
+++ task1.rb
@@ -0,0 +1,20 @@
+def function(array, n)
+  arr = Array.new(array)
+  arr.each do |e|
+    next if e == n
+    buff = arr.each_index.select{|i| arr[i] == e}
+    if buff.length > 1
+      buff.each do |b|
+        arr[b] = n
+      end
+    end
+  end
+  arr
+end
+
+c_before = Array.new(16) { rand(30) }
+c_after =  function(c_before, -1)
+
+c_before.each_index do |i|
+  print c_before[i], "  ==>  ", c_after[i], "\n"
+end
